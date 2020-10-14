79c2f5e92411dd58676e2197a3946e47a1e1319f
DeanKJones <djoneskingsley@artfx.fr>
2020-10-14 19:02:10 +0200

    UI_Button - to-do_UI_draw/update

--- AdventureGame/main.py
+++ AdventureGame/main.py
@@ -2,32 +2,35 @@ import pygame, math
 from sprite import Sprite
 from sprite_controlled import SpriteControlled
 from scene import Scene
-from level00 import Level00
-from level01 import Level01
+#from level00 import Level00
+#from level01 import Level01
 
 def main():
 
     # Load
+
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.mouse.set_visible(False)
     quit_game = False
 
-    level00 = Level00("Background.png", "Ground.png")
-    level01 = Level01("Background.png", "Ground_01.png")
+    level00 = Scene("level00.lvl")
+    level01 = Scene("level01.lvl")
     scenes = {}
     scenes["level00"] = level00
     scenes["level01"] = level01
     current_scene = level00
 
-    def change_scene(name, x):
+    def change_scene(name):
         nonlocal current_scene
         current_scene = scenes[name]
-        current_scene.hero.x = x
+        current_scene.player.x = x
         current_scene.hero.is_moving = False
 
     while not quit_game:
+
         # Inputs
+
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -38,9 +41,11 @@ def main():
         current_scene.inputs(events)
 
         # Update
+
         current_scene.update(change_scene)
         
         # Draw
+
         screen.fill((0, 0, 0))
         current_scene.draw(screen)
         pygame.display.update()
--- AdventureGame/scene.py
+++ AdventureGame/scene.py
@@ -1,22 +1,122 @@
-import pygame, math
-from sprite import Sprite
+import pygame
 from sprite_controlled import SpriteControlled
+from sprite import Sprite
 from warp import Warp
+from ui_panel import UiPanel
+from ui_group import UiGroup
+#from ui_element import UiElement
+from ui_button import UiButton
 
 class Scene:
-
-    def __init__(self, background_file, ground_file):
-        self.background = Sprite(0, 0, background_file, False)
-        self.ground = Sprite(0, 0, ground_file, False)
     
-    def load(self):
-        pass
+    path = 'D:\ArtFX\pythonAdventure\AdventureGame\\'
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.load(filename)
+
+    def load(self, filename):
+        file = open(Scene.path + filename)
+        data = file.read().splitlines()
+
+        self.panel = UiPanel(0, 0, 800, 100)
+
+        ground_height = 0
+        self.cursor = Sprite(0, 0, 'cursor.png', False)
+        self.sprites = []
+        self.warps = []
+
+        self.ui_top = UiGroup()
+        panel = UiPanel(0, 0, 800, 100)
+        button = UiButton(10, 10, 80, 80)
+        self.ui_top.add_element(panel)
+        self.ui_top.add_element(button)
+
+        for line in data:
+            cell = line.split(";")
+            
+            # Ground
+
+            if(cell[0] == "ground"):
+                self.ground = Sprite(0, 0, cell[1]+".png", False)
+                _, screen_h = pygame.display.get_surface().get_size()
+                ground_height = screen_h - self.ground.surface.get_height()
+                self.ground.y = ground_height
+
+            # Background
+
+            elif(cell[0] == "background"):
+                self.background = Sprite(0, 0, cell[1]+".png", False)
+
+            # Hero
+
+            elif(cell[0] == "hero"):
+                height = 0
+                if cell[3] == "ground":
+                    height = -1
+                self.hero = SpriteControlled(int(cell[2]), height, cell[1]+".png", True, int(cell[4]))
+            
+            # Sprites
+
+            elif(cell[0] == "sprite"):
+                height = 0
+                if cell[3] == "ground":
+                    height = -1
+                sprite = Sprite(int(cell[2]), height, cell[1]+".png", True)
+                self.sprites.append(sprite)
+           
+            # Warps
+
+            elif(cell[0] == "warp"):
+                height = 0
+                if cell[3] == "ground":
+                    height = -1
+                warp = Warp(int(cell[2]), height, cell[1]+".png", False, cell[4])
+                self.warps.append(warp)
+
+        # Set heights
+
+        if(self.hero.y == -1):
+            self.hero.y = ground_height
+        for s in self.sprites:
+            if(s.y == -1):
+                s.y = ground_height
+        for w in self.warps:
+            if(w.y == -1):
+                w.y = ground_height - w.surface.get_height() / 2
 
     def inputs(self, events):
-        pass
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_click = pygame.mouse.get_pos()
+                if(mouse_click[1] > self.ui_top.elements[0].h): #trick to not move player on button click
+                    self.hero.move_to(mouse_click[0])
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_F5]:
+                    self.load(self.filename)
+        self.ui_top.inputs(events)
+                
 
     def update(self, change_scene):
-        pass
+        self.cursor.set_position(pygame.mouse.get_pos())
+        self.hero.update()
+        for w in self.warps:
+            if(self.hero.intersects(w)):
+                change_scene(w.to_scene)
+
+        self.panel.update()
+        self.ui_top.update()
 
     def draw(self, screen):
-        pass
+        self.background.draw(screen)
+        self.ground.draw(screen)
+        for w in self.warps:
+            w.draw(screen)
+        for s in self.sprites:
+            s.draw(screen)
+        self.hero.draw(screen)
+        
+        #self.panel.draw(screen)
+        self.cursor.draw(screen)
+    
